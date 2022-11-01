f5b8dfbebfa4418723f9f0f4c710148913bb2478
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-11-01 14:24:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Created by: Alex Nelson
-// Created on: Oct 2022
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-UNIT3-03-JS-VOLUME-OF-SPHERE/sw.js", {
+    scope: "/ICS20-UNIT3-03-JS-VOLUME-OF-SPHERE/",
   })
 }
 
